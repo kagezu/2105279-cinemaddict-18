1cc05fa12052d8fdd3063bdca9d313537b6f2128
hinto <hinto@rambler.ru>
2022-08-15 22:04:46 +0700

    добавляет вывод данных в попапе

--- src/view/film-details.js
+++ src/view/film-details.js
@@ -4,17 +4,55 @@ import dayjs from 'dayjs';
 const createPoster = (poster) => poster ? `<img class="film-details__poster-img" src="${poster}" alt="">` : '';
 const createAgeRating = (age) => age ? `<p class="film-details__age">${age}+</p>` : '';
 const createTitle = (title) => title ? `<h3 class="film-details__title">${title}</h3>` : '';
-const createOriginalTitle = (title) => title ? `<p class="film-details__title-original">${title}</p>` : '';
+const createOriginalTitle = (title) => title ? `<p class="film-details__title-original">Original: ${title}</p>` : '';
 const createTotalRating = (rating) => rating ? ` <div class="film-details__rating">
               <p class="film-details__total-rating">${rating}</p></div>` : '';
-const createPoster = (poster) => poster ? `<img class="film-details__poster-img" src="${poster}" alt="">` : '';
-const createPoster = (poster) => poster ? `<img class="film-details__poster-img" src="${poster}" alt="">` : '';
-const createPoster = (poster) => poster ? `<img class="film-details__poster-img" src="${poster}" alt="">` : '';
+const createDirector = (director) => director ? `
+            <tr class="film-details__row">
+              <td class="film-details__term">Director</td>
+              <td class="film-details__cell">${director}</td>
+            </tr>` : '';
+const createWriters = (writers) => writers ? `
+            <tr class="film-details__row">
+              <td class="film-details__term">Writers</td>
+              <td class="film-details__cell">${writers.join(', ')}</td>
+            </tr>` : '';
+const createActors = (actors) => actors ? `
+            <tr class="film-details__row">
+              <td class="film-details__term">Actors</td>
+              <td class="film-details__cell">${actors.join(', ')}</td>
+            </tr>` : '';
+const createRelease = (release) => release ? `
+            <tr class="film-details__row">
+              <td class="film-details__term">Release Date</td>
+              <td class="film-details__cell">${dayjs(release).format('DD MMMM YYYY')}</td>
+            </tr>` : '';
+const createRuntime = (runtime) => runtime ? `
+            <tr class="film-details__row">
+              <td class="film-details__term">Runtime</td>
+              <td class="film-details__cell">${runtime}m</td>
+            </tr>` : '';
+const createCountry = () => `
+            <tr class="film-details__row">
+              <td class="film-details__term">Country</td>
+              <td class="film-details__cell">USA</td>
+            </tr>`;
+const createGenre = (genre) => `<span class="film-details__genre">${genre}</span>`;
+const createGenres = (genres) => {
+  const genreString = genres.reduce(createGenre);
+  return genreString ? `<tr class="film-details__row">
+              <td class="film-details__term">Genres</td>
+              <td class="film-details__cell">
+               ${genreString}
+              </td>
+            </tr>` : '';
+};
+const createDescription = (description) => description ? `<p class="film-details__film-description">${description}</p>` : '';
+// const createPoster = (poster) => poster ? `<img class="film-details__poster-img" src="${poster}" alt="">` : '';
 
-const createFilmDetailsTemplate = ({ comments, filmInfo, userDetails }, comments) => {
+const createFilmDetailsTemplate = ({ comments, filmInfo, userDetails }, allComments) => {
   const { title, alternativeTitle, totalRating, poster, ageRating, director, writers, actors, release, runtime, genre, description } = filmInfo;
   const { watchList, alreadyWatched, favorite } = userDetails;
-
   return `<section class="film-details">
   <div class="film-details__inner">
     <div class="film-details__top-container">
@@ -23,60 +61,30 @@ const createFilmDetailsTemplate = ({ comments, filmInfo, userDetails }, comments
       </div>
       <div class="film-details__info-wrap">
         <div class="film-details__poster">
-          <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
-
-          <p class="film-details__age">18+</p>
+          ${createPoster(poster)}
+          ${createAgeRating(ageRating)}
         </div>
 
         <div class="film-details__info">
           <div class="film-details__info-head">
             <div class="film-details__title-wrap">
-              <h3 class="film-details__title">The Great Flamarion</h3>
-              <p class="film-details__title-original">Original: The Great Flamarion</p>
+              ${createTitle(title)}
+              ${createOriginalTitle(alternativeTitle)}
             </div>
-
-            <div class="film-details__rating">
-              <p class="film-details__total-rating">8.9</p>
-            </div>
+            ${createTotalRating(totalRating)}
           </div>
 
           <table class="film-details__table">
-            <tr class="film-details__row">
-              <td class="film-details__term">Director</td>
-              <td class="film-details__cell">Anthony Mann</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Writers</td>
-              <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Actors</td>
-              <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Release Date</td>
-              <td class="film-details__cell">30 March 1945</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Runtime</td>
-              <td class="film-details__cell">1h 18m</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Country</td>
-              <td class="film-details__cell">USA</td>
-            </tr>
-            <tr class="film-details__row">
-              <td class="film-details__term">Genres</td>
-              <td class="film-details__cell">
-                <span class="film-details__genre">Drama</span>
-                <span class="film-details__genre">Film-Noir</span>
-                <span class="film-details__genre">Mystery</span></td>
-            </tr>
+            ${createDirector(director)}
+            ${createWriters(writers)}
+            ${createActors(actors)}
+            ${createRelease(release)}
+            ${createRuntime(runtime)}
+            ${createCountry()}
+            ${createGenres(genre)}
           </table>
-
-          <p class="film-details__film-description">
-            The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
-          </p>
+          
+          ${createDescription(description)}
         </div>
       </div>
 
