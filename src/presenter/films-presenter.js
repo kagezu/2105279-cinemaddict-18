e33471ab4106862de701efe7dd4b70aa687cfacd
hinto <hinto@rambler.ru>
2022-08-18 13:42:04 +0700

    реализует показ и сокрытие попапа

---
+++ src/presenter/films-presenter.js
@@ -0,0 +1,79 @@
+import SortView from '../view/sort-view.js';
+import FilmListView from '../view/film-list-view.js';
+import FilmCardView from '../view/film-card-view.js';
+import FilmsView from '../view/films-view.js';
+import FilmListContainerView from '../view/film-list-container.js';
+import NavigationView from '../view/navigation-view.js';
+import ShowMoreButtonView from '../view/show-more-button.js';
+import FilmDetailsView from '../view/film-details-view.js';
+import { render } from '../render.js';
+
+const COUNT_FILM_CARD = 5;
+const ESCAPE_KEY_NAME = 'Escape';
+
+const isEscapeKey = (evt) => evt.key === ESCAPE_KEY_NAME;
+const siteBodyElement = document.body;
+
+export default class FilmsPresenter {
+  #filmsContainer = new FilmsView();
+  #filmList = new FilmListView();
+  #filmListContainer = new FilmListContainerView();
+  #container;
+  #movies;
+  #comments;
+
+  init = (container, movies, comments) => {
+    this.#container = container;
+    this.#movies = movies;
+    this.#comments = comments;
+
+    render(new NavigationView(), this.#container);
+    render(new SortView(), this.#container);
+    render(this.#filmsContainer, this.#container);
+    render(this.#filmList, this.#filmsContainer.element);
+    render(this.#filmListContainer, this.#filmList.element);
+
+    for (let i = 0; i < COUNT_FILM_CARD; i++) {
+      this.#renderCard(this.#movies[i], this.#comments);
+    }
+
+    render(new ShowMoreButtonView(), this.#filmList.element);
+  };
+
+  #renderCard = (move, comments) => {
+    const cardComponent = new FilmCardView(move);
+    let detailsComponent = null;
+
+    const hideDetailsComponent = () => {
+      siteBodyElement.removeChild(detailsComponent.element);
+      siteBodyElement.classList.remove('hide-overflow');
+    };
+
+    const onWindowKeydown = (evt) => {
+      if (isEscapeKey(evt)) {
+        evt.preventDefault();
+        hideDetailsComponent();
+        window.removeEventListener('keydown', onWindowKeydown);
+      }
+    };
+
+    const viewDetailsComponent = () => {
+      if (siteBodyElement.querySelector('.film-details')) {
+        return;
+      }
+      if (!detailsComponent) {
+        detailsComponent = new FilmDetailsView(move, comments);
+        detailsComponent.element.querySelector('.film-details__close-btn').addEventListener('click', () => {
+          hideDetailsComponent();
+          window.removeEventListener('keydown', onWindowKeydown);
+        });
+      }
+      siteBodyElement.classList.add('hide-overflow');
+      siteBodyElement.appendChild(detailsComponent.element);
+      window.addEventListener('keydown', onWindowKeydown);
+    };
+
+    cardComponent.element.querySelector('.film-card__link').addEventListener('click', () => viewDetailsComponent());
+    render(cardComponent, this.#filmListContainer.element);
+  };
+}
