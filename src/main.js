63a300e31baea9aadcc28c78ffb291971ee56ea9
hinto <hinto@rambler.ru>
2022-08-18 14:34:56 +0700

    Удаляет закоментированные строки

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import ProfileView from './view/profile-view.js';
-// import FilmDetailsView from './view/film-details-view.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import { render } from './render.js';
 import MovieModel from './model/movie-model.js';
@@ -7,7 +6,6 @@ import CommentsModel from './model/comments-model.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
-// const siteBodyElement = document.querySelector('body');
 
 const movieModel = new MovieModel();
 const commentsModel = new CommentsModel();
@@ -15,4 +13,3 @@ const commentsModel = new CommentsModel();
 render(new ProfileView(), siteHeaderElement);
 const filmsPresenter = new FilmsPresenter();
 filmsPresenter.init(siteMainElement, movieModel.get(), commentsModel.get());
-// render(new FilmDetailsView(movieModel.get()[0], commentsModel.get()), siteBodyElement);
