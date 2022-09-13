7a3e95aeca52483e0f1cc857d50773f062f460ce
hinto <hinto@rambler.ru>
2022-09-13 22:20:47 +0700

    Добавляет модуль для работы с API

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import MovieModel from './model/movie-model.js';
 import CommentsModel from './model/comments-model.js';
 import FilterModel from './model/filter-model.js';
 import ProfileView from './view/profile-view.js';
+import FilmsApiService from './films-api-service.js';
+
+const AUTHORIZATION = 'Basic hf08954hw578h75f0';
+const END_POINT = 'https://18.ecmascript.pages.academy/cinemaddict';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 
-const movieModel = new MovieModel();
+const movieModel = new MovieModel(new FilmsApiService(END_POINT, AUTHORIZATION));
 const commentsModel = new CommentsModel();
 const filterModel = new FilterModel();
 
--- src/model/movie-model.js
+++ src/model/movie-model.js
@@ -2,7 +2,23 @@ import { generateMovie } from '../mock/movie.js';
 import Observable from '../framework/observable.js';
 
 export default class MovieModel extends Observable {
+  #moviesApiService = null;
+
   #movies = Array.from({ length: 22 }, generateMovie);
+
+  constructor(moviesApiService) {
+    super();
+    this.#moviesApiService = moviesApiService;
+
+    this.#moviesApiService.movies.then((movies) => {
+      console.log(movies);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
+
 
   get movies() {
     return this.#movies;
