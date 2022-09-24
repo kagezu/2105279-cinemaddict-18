614961128badbe72ac5ef9aeb0ed3294756b400e
hinto <hinto@rambler.ru>
2022-09-24 14:18:05 +0700

    Исправляет функции сортировки

--- src/utils/common.js
+++ src/utils/common.js
@@ -12,6 +12,6 @@ export const transformFirstCharToUpperCase = (str) => `${str[0].toUpperCase()}${
 export const isEscapeKey = (evt) => evt.key === ESCAPE_KEY_NAME;
 
 // Функции сортировки
-export const sortDate = (a, b) => dayjs(b.filmInfo.release.date).diff(a.filmInfo.release.date) > 0;
-export const sortRating = (a, b) => a.filmInfo.totalRating < b.filmInfo.totalRating;
-export const sortComment = (a, b) => a.comments?.length < b.comments?.length;
+export const sortDate = (a, b) => dayjs(b.filmInfo.release.date).diff(a.filmInfo.release.date);
+export const sortRating = (a, b) => b.filmInfo.totalRating - a.filmInfo.totalRating;
+export const sortComment = (a, b) => b.comments?.length - a.comments?.length;
